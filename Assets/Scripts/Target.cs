ed55fa6ad1e1ffb9ad8b7c9a2eeddfb6772a1a00
AnnitaMele <annitadraw18@gmail.com>
2020-04-16 22:23:52 -0500

    Initial commit

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Target : MonoBehaviour
+{
+    private const float TIME_TO_DESTROY = 10F;
+
+    [SerializeField]
+    private int maxHP = 1;
+
+    private int currentHP;
+
+    [SerializeField]
+    private int scoreAdd = 10;
+
+    private void Start()
+    {
+        currentHP = maxHP;
+        Destroy(gameObject, TIME_TO_DESTROY);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        int collidedObjectLayer = collision.gameObject.layer;
+        if (collidedObjectLayer.Equals(Utils.BulletLayer))
+        {
+            //Destroy(collision.gameObject);
+
+            TargetDamage();
+
+
+        }
+        else if (collidedObjectLayer.Equals(Utils.PlayerLayer) ||
+            collidedObjectLayer.Equals(Utils.KillVolumeLayer))
+        {
+
+        }
+
+
+        if (collidedObjectLayer.Equals(Utils.PlayerLayer) ||
+            collidedObjectLayer.Equals(Utils.KillVolumeLayer))
+        {
+            Player.instance.DamagePlayer();
+
+        }
+
+    }
+
+    public void TargetDamage()
+    {
+        currentHP -= 1;
+        if (currentHP <= 0)
+        {
+            Player.instance.ScoreP(scoreAdd);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image[] lifeImages;
+
+    [SerializeField]
+    private Text scoreLabel;
+
+    [SerializeField]
+    private Button restartBtn;
+
+    
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        ToggleRestartButton(false);
+        Player.instance.OnPlayerDied += Death;
+        Player.instance.OnPlayerHit += PlayerLivesCount;
+        Player.instance.OnPlayerScoreC += ScorePlayerCount;
+        
+    }
+
+    private void ToggleRestartButton(bool val)
+    {
+        if (restartBtn != null)
+        {
+            restartBtn.gameObject.SetActive(val);
+        }
+    }
+
+   
+    public void Death()
+    {
+
+    }
+
+    public void PlayerLivesCount(int updateLives)
+    {
+        if (!scoreLabel || lifeImages != null) return;
+
+        if (updateLives <= 0)
+            scoreLabel.text = "Game Over";
+        for (int i = 0; i < lifeImages.Length; i++)
+        {
+            lifeImages[i].enabled = i < updateLives;
+        }
+    }
+
+    public void ScorePlayerCount(int updateScore)
+    {
+        if (!scoreLabel) return;
+        scoreLabel.text = updateScore.ToString();
+    }
+    
+}
